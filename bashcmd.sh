7d1853d62ed5743e60309e24ecd3e692dd4524ed
Siva Krishna Bungatavula <61808642+sivakris14@users.noreply.github.com>
2026-05-09 16:19:16 +0530

    Update bashcmd.sh

--- bashcmd.sh
+++ bashcmd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "this is  script display time everytime  $(date)"
+#Project https://flaxen-diplodocus-1a1.notion.site/Roboshop-Procedure-a697a31731314422842461e1269ad236
 echo "system load $(uptime)"
 # https://www.youtube.com/watch?v=o6ll5SviMKY&list=PLPWyJYiAyYNtTzrVPCd960LeMjjhU4Cuz&index=2
 # https://tally.so/r/3XL95L
