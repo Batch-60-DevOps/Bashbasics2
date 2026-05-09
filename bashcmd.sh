772169c38ce6e0f47f1072546dcac4a96ef508fb
Siva Krishna Bungatavula <61808642+sivakris14@users.noreply.github.com>
2026-05-09 07:05:52 +0530

    Modify comments in bashcmd.sh
    Updated comment to include an additional URL.

--- bashcmd.sh
+++ bashcmd.sh
@@ -2,4 +2,5 @@
 echo "hi this is my first bash script"
 echo "this is  script display time everytime  $(date)"
 echo "system load $(uptime)"
-#https://www.youtube.com/watch?v=o6ll5SviMKY&list=PLPWyJYiAyYNtTzrVPCd960LeMjjhU4Cuz&index=2
+# https://www.youtube.com/watch?v=o6ll5SviMKY&list=PLPWyJYiAyYNtTzrVPCd960LeMjjhU4Cuz&index=2
+# https://tally.so/r/3XL95L
