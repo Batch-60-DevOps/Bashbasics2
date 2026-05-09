bb3fe1e4a284ae565acfe80616db9329ba17ae30
Siva Krishna Bungatavula <61808642+sivakris14@users.noreply.github.com>
2026-05-09 07:06:19 +0530

    Remove initial greeting from bash script
    Removed the initial greeting message from the script.

--- bashcmd.sh
+++ bashcmd.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "hi this is my first bash script"
 echo "this is  script display time everytime  $(date)"
 echo "system load $(uptime)"
 # https://www.youtube.com/watch?v=o6ll5SviMKY&list=PLPWyJYiAyYNtTzrVPCd960LeMjjhU4Cuz&index=2
