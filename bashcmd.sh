278bcb51e39cab1bd0b6fca30c8d95350e0b2179
Siva Krishna Bungatavula <61808642+sivakris14@users.noreply.github.com>
2026-05-09 07:01:01 +0530

    Add comment with YouTube link to bash script
    Added a comment with a YouTube link for reference.

--- bashcmd.sh
+++ bashcmd.sh
@@ -2,3 +2,4 @@
 echo "hi this is my first bash script"
 echo "this is  script display time everytime  $(date)"
 echo "system load $(uptime)"
+#https://www.youtube.com/watch?v=o6ll5SviMKY&list=PLPWyJYiAyYNtTzrVPCd960LeMjjhU4Cuz&index=2
