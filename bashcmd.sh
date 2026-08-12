9184acebbc7801af11ee0da8ffb2df6d80016c29
Siva Krishna Bungatavula <61808642+sivakris14@users.noreply.github.com>
2026-08-12 17:09:24 +0530

    Update bashcmd.sh

--- bashcmd.sh
+++ bashcmd.sh
@@ -3,5 +3,6 @@
 echo "system load $(uptime)"
 # https://www.youtube.com/watch?v=o6ll5SviMKY&list=PLPWyJYiAyYNtTzrVPCd960LeMjjhU4Cuz&index=2
 # https://tally.so/r/3XL95L
+https://us06web.zoom.us/j/84471181106?pwd=nEOZRFalQuXaVnZgzTNTcylaNbObsc.1
 # https://github.com/b60-clouddevops
 https://coverr.co
